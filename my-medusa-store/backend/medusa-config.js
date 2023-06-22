f313cccb9639a579de2ad6512cb5ea05a85ae475
­이희원(엘텍공과대학 소프트웨어학부) <heewonlee@ewhain.net>
2023-06-22 18:34:14 +0900

    feat: add custom route

--- my-medusa-store/backend/medusa-config.js
+++ my-medusa-store/backend/medusa-config.js
@@ -23,10 +23,10 @@ try {
 
 // CORS when consuming Medusa from admin
 const ADMIN_CORS =
-  process.env.ADMIN_CORS || "http://192.168.5.41:7000,http://192.168.5.41:7001,http://211.178.253.50:7000,http://localhost:7000";
+  process.env.ADMIN_CORS || "http://192.168.5.41:7000,http://192.168.5.41:7001,http://211.178.253.50:7000,http://localhost:7000,http://localhost:9000";
 
 // CORS to avoid issues when consuming Medusa from a client
-const STORE_CORS = process.env.STORE_CORS || "http://192.168.5.41:8000,http://localhost:8000,http://211.178.253.50:7000,http://localhost:7000";
+const STORE_CORS = process.env.STORE_CORS || "http://192.168.5.41:8000,http://localhost:8000,http://211.178.253.50:7000,http://localhost:7000,http://localhost:8001";
 
 const DATABASE_URL =
   process.env.DATABASE_URL || "postgres://localhost/medusa-store";
--- my-medusa-store/backend/src/api/index.ts
+++ my-medusa-store/backend/src/api/index.ts
@@ -1,6 +1,119 @@
 import { Router } from "express"
+import Medusa from "@medusajs/medusa-js"
+import { stat } from "fs";
 
 export default (rootDirectory: string): Router | Router[] => {
   // add your custom routes here
-  return []
+  const router = Router()
+  const medusa = new Medusa({ baseUrl: "http://localhost:9000", maxRetries: 3, apiKey: "hello" });
+
+  router.get("/products", (req, res) => {
+    let responseData = {}
+    medusa.admin.products.list()
+    .then(({ products, limit, offset, count }) => {
+      res.send(products);
+      for (let i = 0; i<count; i++) {
+        console.log(products[i].id);
+      }
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+  });
+
+  router.get("/product/:productNo", (req, res) => {
+    const productId = req.params.productNo;
+    let responseData = {}
+    medusa.admin.products.retrieve(productId)
+    .then(({ product }) => {
+      res.send(product);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+  });
+
+  router.post("/product/:title", (req, res) => {
+    const title = req.params.title;
+    const subtitle = req.body.subtitle;
+    console.log(typeof subtitle);
+    const description = req.body.description;
+    const categories = req.body.categories;
+    const imageList = req.body.images;
+    const status = req.body.status;
+    const options = req.body.options;
+    const variants = req.body.variants;
+    let responseData = {}
+    console.log(typeof subtitle);
+    // medusa.admin.products.create({
+    //   title: title,
+    //   subtitle: subtitle,
+    //   description: description,
+    //   categories: categories,
+    //   is_giftcard: false,
+    //   discountable: false,
+    //   images: imageList,
+    //   status: status,
+    //   options: options,
+    //   variants: variants,
+    // })
+    // .then(({ product }) => {
+    //   res.send(product.id);
+    // })
+    // .catch((err) => {
+    //   res.status(500).send(err);
+    // });
+  });
+
+  router.put("/product/:title", (req, res) => {
+    const title = req.params.title;
+    const subtitle = req.body.subtitle;
+    const imageList = req.body.images;
+    const status = req.body.status;
+    const options = req.body.options;
+    const variants = req.body.variants;
+    let responseData = {}
+    medusa.admin.products.create({
+      title: title,
+      subtitle: subtitle,
+      is_giftcard: false,
+      discountable: false,
+      images: imageList,
+      status: status,
+      options: options,
+      variants: variants,
+    })
+    .then(({ product }) => {
+      res.send(product.id);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+  });
+
+  router.delete("/product/:productNo", (req, res) => {
+    const productNo = req.params.productNo;
+    medusa.admin.products.delete(productNo)
+    .then(({ id, object, deleted }) => {
+      if (deleted) {
+        console.log(id);
+        res.send(id);
+      } else {
+        throw new Error("delete fail");
+      }
+    })
+    .catch((err) => {
+      if (err.message === "delete fail") {
+        let result = {
+          code: 'FAIL',
+          message: err.message,
+        };
+        res.status(400).send(result);
+      } else {
+        res.status(500).send(err);
+      }
+    });
+  })
+
+  return [router]
 }
